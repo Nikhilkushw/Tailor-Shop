621ae9185eb87e2c48cfa37bfbb4367d85ffb088
Nikhil Kushwah <nikhilkush037@gmail.com>
2025-09-15 20:46:34 +0530

    update tailor code

--- frontend/src/components/Offers.jsx
+++ frontend/src/components/Offers.jsx
@@ -10,7 +10,7 @@ const Offers = () => {
   const getOffers = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:5000/api/offers"
+        "https://tailor-shop-1.onrender.com/api/offers"
       );
       console.log("Offers fetched:", response.data);
 
